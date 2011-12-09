45141f396eb4c6f5699c8cde5130006b508153e7
Tony Ng <tonyng222@gmail.com>
2011-12-09 14:32:30 -0800

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Star::Application.config.secret_token = '0486548fe148ab269f82460f615e28137518173f6d685c7e1b72bef58d010a2b6589884bdc052e031d2624ce1455857c7b2005f3da91bc15779982d56a7c412a'
